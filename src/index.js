ccb699a1020d839df61480fe9d25ae7b45ef3641
Jelani Cannon <jelani.cannon@gmail.com>
2022-10-24 08:56:04 -0700

    commit tree 0ee4981f97aed19ceedefaf87846dd4e22db9dc0

--- src/index.js
+++ src/index.js
@@ -1,1 +1,55 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", function(){
+    getDogs() 
+    getBreeds() 
+    
+})
+
+
+
+function renderImages(dogs){
+    dogs.forEach(function(dog){
+        const container = document.getElementById("dog-image-container")
+        const doggyImage = document.createElement('img')
+        doggyImage.setAttribute('src', `${dog}`)
+        doggyImage.setAttribute('alt', "awww pwetty doggy")
+        container.appendChild(doggyImage)
+    })
+}
+
+function renderBreeds(breeds){
+    breeds.forEach(function(breed){
+        const breedList = document.getElementById('dog-breeds')
+        const li = document.createElement('li')
+        li.innerText=`${breed}`
+        li.setAttribute('class', "breeds")
+        li.addEventListener('click', function(){
+            console.log("I have been clicked")
+            li.style.color = 'blue'
+            
+            
+        })
+        breedList.appendChild(li)
+    })
+}
+
+function getDogs (){
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then((resp)=> resp.json())
+    .then((json)=> renderImages(json.message))
+}
+function getBreeds (){
+    fetch("https://dog.ceo/api/breeds/list/all")
+    .then((resp) => resp.json())
+    .then((json)=> renderBreeds(Object.keys(json.message)))
+}
+
+function filterDogs(){
+    //let options = document.getElementById('breed-dropdown')
+    const breedList = document.getElementsByClassName('breed')
+   for (const breed of breedList){
+        console.log(breed)
+    }
+}
+filterDogs()
